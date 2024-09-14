e56577508a187139d38a3b82fce4e3c343119ef9
FW-Titi <136483262+FW-Titi@users.noreply.github.com>
2024-09-14 12:51:42 -0400

    Updated seo

--- libs/seo.js
+++ libs/seo.js
@@ -50,8 +50,8 @@ export const getSEOTags = ({
       description: openGraph?.description || config.appDescription,
       // If you add an twitter-image.(jpg|jpeg|png|gif) image to the /app folder, you don't need the code below
       // images: [openGraph?.image || defaults.og.image],
-      card: "summary_large_image",
-      creator: "@marc_louvion",
+      card: "",
+      creator: "",
     },
 
     // If a canonical URL is given, we add it. The metadataBase will turn the relative URL into a fully qualified URL
@@ -85,10 +85,10 @@ export const renderSchemaTags = () => {
           url: `https://${config.domainName}/`,
           author: {
             "@type": "Person",
-            name: "Marc Lou",
+            name: "Matt Titi",
           },
-          datePublished: "2023-08-01",
-          applicationCategory: "EducationalApplication",
+          datePublished: "2024-08-01",
+          applicationCategory: "SoftwareApplication",
           aggregateRating: {
             "@type": "AggregateRating",
             ratingValue: "4.8",
@@ -97,7 +97,7 @@ export const renderSchemaTags = () => {
           offers: [
             {
               "@type": "Offer",
-              price: "9.00",
+              price: "9.99",
               priceCurrency: "USD",
             },
           ],
